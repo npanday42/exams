b22517adbff706ea385a0a83608401f189c72e79
Noël <npanday@student.codam.nl>
2019-05-21 13:09:53 +0200

    Working

--- rpn_calc.c
+++ rpn_calc.c
@@ -6,7 +6,7 @@
 /*   By: npanday <npanday@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/20 16:52:11 by npanday        #+#    #+#                */
-/*   Updated: 2019/05/21 11:33:01 by npanday       ########   odam.nl         */
+/*   Updated: 2019/05/21 13:03:01 by npanday       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,50 @@ static int	is_op(char c)
 	return (1);
 }
 
-static int	isdigit(char c)
+static int	ft_isdigit(char c)
 {
 	return (c >= '0' && c <= '9');
 }
 
-static int	rpn_calc(char *str, char op)
+static int	rpn_calc(char *str);
+
+static int	process(char *str)
+{
+	char c = str[i];
+
+	i--;
+	if (c == '+')
+		return (rpn_calc(str) + rpn_calc(str));
+	if (c == '-')
+		return (rpn_calc(str) - rpn_calc(str));
+	if (c == '*')
+		return (rpn_calc(str) * rpn_calc(str));
+	if (c == '/')
+		return (rpn_calc(str) / rpn_calc(str));
+	if (c == '%')
+		return (rpn_calc(str) % rpn_calc(str));
+	return (0);
+}
+
+static int	rpn_calc(char *str)
 {
 	char	num = 0;
 
-	if (op != 0 && str[i] != ' ')
+	if (str[i] != 0 && str[i] != ' ')
 		error = 1;
 	while (i > 0 && !error)
 	{
 		i--;
 		if (num && str[i] == ' ')
 			return (atoi(str + i + 1));
-		else if (isdigit(str[i]))
+		else if (ft_isdigit(str[i]))
 			num = 1;
 		else if (is_op(str[i]))
-			return (process());
+			return (process(str));
 		else if (str[i] != ' ')
 			error = 1;
 	}
-	if (i == 0 && !isdigit(str[i]))
+	if (i == 0 && !ft_isdigit(str[i]))
 		error = 1;
 	if (error)
 		return (0);
@@ -66,8 +86,8 @@ int			main(int argc, char **argv)
 		i++;
 	error = (argc != 2);
 	if (!error)
-		ret = rpn_calc(argv[1], 0);
-	if (error)
+		ret = rpn_calc(argv[1]);
+	if (error || i != 0)
 		return (printf("Error\n"));
 	return (printf("%d\n", ret));
 }
