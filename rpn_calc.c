d606eae5b0098174374d4e3e01a6cba085136889
Noël <npanday@student.codam.nl>
2019-05-26 23:30:35 +0200

    Simplified

--- rpn_calc.c
+++ rpn_calc.c
@@ -6,7 +6,7 @@
 /*   By: npanday <npanday@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/20 16:52:11 by npanday        #+#    #+#                */
-/*   Updated: 2019/05/26 22:38:27 by npanday       ########   odam.nl         */
+/*   Updated: 2019/05/26 23:29:38 by npanday       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,7 @@ static int	process(char c)
 		return (a / b);
 	if (c == '%')
 		return (a % b);
-	error = 1;
-	return (1);
+	return ((error = 1));
 }
 
 static int	rpn_calc(void)
@@ -50,20 +49,20 @@ static int	rpn_calc(void)
 	char	num = 0;
 
 	if (str[i] != 0 && str[i] != ' ')
-		error = 1;
-	while (i > 0 && !error)
+		return ((error = 1));
+	while (i > 0)
 	{
 		i--;
 		if (ft_isdigit(str[i]))
 			num = 1;
 		else if (i == 0)
-			error = 1;
+			return ((error = 1));
 		else if (str[i] != ' ')
 			return (process(str[i--]));
 		else if (num)
 			return (atoi(str + i + 1));
 	}
-	return (error ? 1 : atoi(str + i));
+	return (atoi(str + i));
 }
 
 int			main(int argc, char **argv)
