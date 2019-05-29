82bc9fe0402ad289c9e57c6180b5cb8043370d0f
Noel Panday <npanday@f0r4s13.codam.nl>
2019-05-29 19:22:58 +0200

    Working

--- print_memory.c
+++ print_memory.c
@@ -1,31 +1,72 @@
 #include <unistd.h>
+#define HEX "0123456789abcdef"
 
-// void	print_memory(const void *addr, size_t size)
-// {
-// 	int		i;
+typedef unsigned char t_byte;
 
-// 	i = 0;
-// 	while (i < size)
-// 	{
-// 		write(STDOUT_FILENO, )
-// 	}
-// }
-
-int		main(void)
+static void	print_chars(const void *addr, size_t size)
 {
-	int		i;
-	char	*c;
+	char	*str;
+	size_t	i;
 
+	str = (char *)addr;
 	i = 0;
-	c = 30;
-	while (i < 128)
+	while (i < size)
 	{
-		write(1, c + i, 1);
-		if ((i + 1) % 16 == 0)
-			write(1, "\n", 1);
-		else if (i < 255)
-			write(1, "-", 1);
+		if (str[i] >= 32 && str[i] <= 126)
+			write(1, str + i, 1);
+		else
+			write(1, ".", 1);
 		i++;
 	}
-	return (0);
+}
+
+static void	print_hex(const void *addr, size_t size)
+{
+	t_byte	*str;
+	size_t	i;
+
+	str = (t_byte *)addr;
+	i = 0;
+	while (i < size)
+	{
+		write(1, &HEX[str[i] / 16], 1);
+		write(1, &HEX[str[i] % 16], 1);
+		if ((i + 1) % 2 == 0)
+			write(1, " ", 1);
+		i++;
+	}
+	while (i < 16)
+	{
+		write(1, "  ", 2);
+		if ((i + 1) % 2 == 0)
+			write(1, " ", 1);
+		i++;
+	}
+}
+
+static void	print_memory(const void *addr, size_t size)
+{
+	size_t	i;
+	size_t	line_size;
+
+	i = 0;
+	while (i < size)
+	{
+		line_size = (size - i >= 16) ? 16 : size - i;
+		print_hex(addr + i, line_size);
+		print_chars(addr + i, line_size);
+		write(1, "\n", 1);
+		i += 16;
+	}
+}
+
+int			main(void)
+{
+	int		tab[10] = {0, 23, 150, 255, 12, 16, 42, 103};
+	char	chars[] = "Hello, World!";
+
+	print_memory(tab, sizeof(tab));
+	write(1, "\n", 1);
+	print_memory(chars, sizeof(chars) - 1);
+	return (1);
 }
